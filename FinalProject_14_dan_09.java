89d3793fc31bb5b8c91eef2a7ed07c8f22ab0dd6
RezzAja <f4khrurrozy.22nkh@gmail.com>
2025-09-24 14:41:31 +0700

    1

--- FinalProject_14_dan_09.java
+++ FinalProject_14_dan_09.java
@@ -1,7 +1,8 @@
 public class FinalProject_14_dan_09 {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama yang di modifikasi oleh user 2");
-        System.out.println("Briss ke dua dari user ke 2");
+        System.out.println("Briss ke dua dari user ke 1");
+        
 
     }
 } 
